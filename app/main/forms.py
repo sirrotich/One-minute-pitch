789a717ce242b92dfb61fce0763c8ba12612ddd5
sirrotich <rotichtitus12@gmail.com>
2019-04-21 17:46:16 +0300

    create forms in forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
 
-class RegistrationForm(FlaskForm):
-    
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title',validators=[Required()])
+    category = SelectField(u'Pitch Category', choices = [('life', 'life'), ('coding', 'coding'), ('funny', 'funny')])
+    pitch = TextAreaField('Pitch')
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+
+    comment = TextAreaField('Comment')
+    submit = SubmitField('Submit') 
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you', validators=[Required()])
+    submit = SubmitField('Submit')
