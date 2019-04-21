8605cbb03e62ba7ba76eaabab3988690213357e2
sirrotich <rotichtitus12@gmail.com>
2019-04-21 10:59:29 +0300

    import the db and add a user class that takes in the db.model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+from . import db
+
+class User(db.model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    author = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.author}'
+        
 class new_pitch:
     def __init__(title,pitch,author,):
         self.title = title
