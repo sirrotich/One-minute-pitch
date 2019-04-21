421c68670d716f9f31d25ede9341f7b816160cfc
sirrotich <rotichtitus12@gmail.com>
2019-04-21 20:35:51 +0300

    define a function that takes the regestration form

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,3 +19,22 @@ def login():
 
     title = "Pitches login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+         author = form.author.data,
+         password = form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Pitch It Up!","email/welcome_user",user.email,user=user)
+
+        title = "New Account"
+
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html',registration_form = form)
