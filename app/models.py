ba6323127202cc187769cc6b54bdb4281081b8b5
sirrotich <rotichtitus12@gmail.com>
2019-04-21 21:43:08 +0300

    add pitch class"

--- app/models.py
+++ app/models.py
@@ -31,13 +31,29 @@ class User(UserMixin,db.Model):
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
 
+
     def __repr__(self):
         return f'Author: {self.author}'
 
+class Pitches(db.Model):
+    __tablename__= 'pitches'
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255))
+    category = db.Column(db.String(255))
+    pitch = db.Column(db.String(255))
+    date = db.Column(db.DateTime(250), default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comments = db.relationship('Comments', backref='title', lazy='dynamic')
 
-class new_pitch:
-    def __init__(title,pitch,author,):
-        self.title = title
-        self.pitch = pitch
-        self.author = author
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls,cate):
+        pitch = Pitches.query.filter_by(category=cate).all()
+        return pitch
+
+    def __repr__(self):
+        return f"Pitches {self.pitch}','{self.date}')"
         
