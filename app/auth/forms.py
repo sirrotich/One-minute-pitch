b4ce7dcacf7cdb2f074b198b5c2bf4282cc08795
sirrotich <rotichtitus12@gmail.com>
2019-04-21 20:27:32 +0300

    create login form class to take the inputs and validations

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -17,3 +17,9 @@ class RegistrationForm(FlaskForm):
     def validate_author(self,data_field):
         if User.query.filter_by(author = data_field.data).first():
             raise ValidationError('That author name is taken')
+        
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
