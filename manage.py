bbb15629edfafd560052e91f60cecec0555fe033
sirrotich <rotichtitus12@gmail.com>
2019-04-21 22:22:10 +0300

    modify manage.py file

--- manage.py
+++ manage.py
@@ -1,11 +1,25 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
+from app.models import User,Pitches, Comments
+
 
 # Creating app instance
 app = create_app('development')
 
-manager = Manager(app)
-manager.add_command('server',Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Pitches = Pitches, Comments= Comments)
+
+manager = Manager(app,db)
+manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
     manage.run()
