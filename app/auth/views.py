44fd11646caefad80e5c4c555f5300d4e749df0c
sirrotich <rotichtitus12@gmail.com>
2019-04-21 20:37:28 +0300

    check indentations and create a logout function

--- app/auth/views.py
+++ app/auth/views.py
@@ -20,6 +20,7 @@ def login():
     title = "Pitches login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
+
 @auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
@@ -38,3 +39,10 @@ def register():
         return redirect(url_for('auth.login'))
 
     return render_template('auth/register.html',registration_form = form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
