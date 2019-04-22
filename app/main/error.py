0993043b2239ad3dda2b5a4c5c2af4e800f043ce
sirrotich <rotichtitus12@gmail.com>
2019-04-22 16:20:03 +0300

    create a register html file and make changes to nav bar

--- app/main/error.py
+++ app/main/error.py
@@ -2,8 +2,8 @@ from flask import render_template
 from . import main
 
 @main.app_errorhandler(404)
-def four_Ow_four(error):
+def four_Oh_four(error):
     '''
     Function to render the 404 error page
     '''
-    return render_template('fourOwfour.html'),404
+    return render_template('fourOhfour.html'),404
