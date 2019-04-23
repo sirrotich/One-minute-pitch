01e45c1f3b59cac32cd0b881968f3781d668f8c5
sirrotich <rotichtitus12@gmail.com>
2019-04-23 11:02:13 +0300

    add tests

--- app/tests/test_comment.py
+++ app/tests/test_comment.py
@@ -4,7 +4,7 @@ from app import db
 
 def setUp(self):
         self.user_Rotich = User(username = 'Rotich',password = 'potato', email = 'rotichtitus12@gmail.com')
-        self.new_comment = Comment(pitch_title='Comment for pitchs',image_path="https://image.tmdb.org/t/p/w500/jdjdjdjn",pitch_comment='This pitch is the best thing since sliced bread',user = self.user_Rotich )
+        self.new_comment = Comment(pitch_title='movie',pitch="the heritage was from a history",pitch_comment='This pitch is the best thing since sliced bread',user = self.user_Rotich )
 
 def tearDown(self):
         Comment.query.delete()
@@ -12,7 +12,7 @@ def tearDown(self):
 
 def test_check_instance_variables(self):
         self.assertEquals(self.new_comment.pitch_title,'Comment for pitches')
-        self.assertEquals(self.new_comment.image_path,"https://image.tmdb.org/t/p/w500/jdjdjdjn")
+        self.assertEquals(self.new_comment.pitch,"the heritage was from a history")
         self.assertEquals(self.new_comment.pitch_comment,'This pitch is the best thing since sliced bread')
         self.assertEquals(self.new_Comment.user,self.user_Rotich)
 
